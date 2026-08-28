72b187cbc2dd9e11a8cc915c3d36c52ef284f433
HBHATIA80 <61718992+HBHATIA80@users.noreply.github.com>
2026-08-28 19:44:51 +0530

    Load adaptive theme as final global UI layer

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,7 @@ import './ux-hardening.css'
 import './final-ui.css'
 import './ui-refresh.css'
 import './ui-final.css'
+import './adaptive-theme.css'
 import ClickAwayCloser from './dashboard/click-away-closer'
 import { Toaster } from 'react-hot-toast'
 
