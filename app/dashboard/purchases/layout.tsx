286460d87353891e60e7fcf1cab3a6737be0358c
HBHATIA80 <61718992+HBHATIA80@users.noreply.github.com>
2026-08-31 08:27:41 +0530

    Apply targeted latest purchase voucher modal polish

--- app/dashboard/purchases/layout.tsx
+++ app/dashboard/purchases/layout.tsx
@@ -1,7 +1,8 @@
 import reference from '../transaction-voucher-reference.module.css'
 import typography from './purchase-typography.module.css'
+import voucherModal from '../latest-voucher-modal.module.css'
 import LatestVoucherScreen from '../LatestVoucherScreen'
 
 export default function PurchasesLayout({ children }: { children: React.ReactNode }) {
-  return <div className={`${reference.transactionRoot} ${typography.purchaseTypography}`}>{children}<LatestVoucherScreen /></div>
+  return <div className={`${reference.transactionRoot} ${typography.purchaseTypography} ${voucherModal.voucherModalScope}`}>{children}<LatestVoucherScreen /></div>
 }
