3e0a8a8d85172219112dda7d08165f0d25e7134e
HBHATIA80 <61718992+HBHATIA80@users.noreply.github.com>
2026-08-31 08:20:28 +0530

    Enable detailed purchase voucher screen

--- app/dashboard/purchases/layout.tsx
+++ app/dashboard/purchases/layout.tsx
@@ -1,6 +1,7 @@
 import reference from '../transaction-voucher-reference.module.css'
 import typography from './purchase-typography.module.css'
+import LatestVoucherScreen from '../LatestVoucherScreen'
 
 export default function PurchasesLayout({ children }: { children: React.ReactNode }) {
-  return <div className={`${reference.transactionRoot} ${typography.purchaseTypography}`}>{children}</div>
+  return <div className={`${reference.transactionRoot} ${typography.purchaseTypography}`}>{children}<LatestVoucherScreen /></div>
 }
