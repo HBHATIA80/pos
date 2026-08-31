286ce2ec5bf109319dbc0a10c85572578cb4cc53
HBHATIA80 <61718992+HBHATIA80@users.noreply.github.com>
2026-08-31 08:03:20 +0530

    Apply purchase POS typography polish

--- app/dashboard/purchases/layout.tsx
+++ app/dashboard/purchases/layout.tsx
@@ -1,5 +1,6 @@
 import reference from '../transaction-voucher-reference.module.css'
+import typography from './purchase-typography.module.css'
 
 export default function PurchasesLayout({ children }: { children: React.ReactNode }) {
-  return <div className={reference.transactionRoot}>{children}</div>
+  return <div className={`${reference.transactionRoot} ${typography.purchaseTypography}`}>{children}</div>
 }
