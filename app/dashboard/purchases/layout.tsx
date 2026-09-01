539f6275e34d392086995b637c08be7c6a94c6de
HBHATIA80 <61718992+HBHATIA80@users.noreply.github.com>
2026-09-01 17:28:26 +0530

    Apply mobile voucher styles to purchase

--- app/dashboard/purchases/layout.tsx
+++ app/dashboard/purchases/layout.tsx
@@ -1,8 +1,9 @@
 import reference from '../transaction-voucher-reference.module.css'
 import typography from './purchase-typography.module.css'
 import voucherModal from '../latest-voucher-modal.module.css'
+import mobile from '../mobile-voucher.module.css'
 import LatestVoucherScreen from '../LatestVoucherScreen'
 
 export default function PurchasesLayout({ children }: { children: React.ReactNode }) {
-  return <div className={`${reference.transactionRoot} ${typography.purchaseTypography} ${voucherModal.voucherModalScope}`}>{children}<LatestVoucherScreen /></div>
+  return <div className={`${reference.transactionRoot} ${typography.purchaseTypography} ${voucherModal.voucherModalScope} ${mobile.mobileVoucher}`}>{children}<LatestVoucherScreen /></div>
 }
