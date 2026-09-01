6e7eb1cb653fbddd0d771ee0faeb55bcc35ef651
HBHATIA80 <61718992+HBHATIA80@users.noreply.github.com>
2026-09-01 14:27:27 +0530

    Apply purchase-voucher styling to sales workspace

--- app/dashboard/sales/layout.tsx
+++ app/dashboard/sales/layout.tsx
@@ -1,8 +1,9 @@
 import styles from './sales.module.css'
+import matchStyles from './sales-purchase-match.module.css'
 import reference from '../transaction-voucher-reference.module.css'
 import voucherModal from '../latest-voucher-modal.module.css'
 import LatestVoucherScreen from '../LatestVoucherScreen'
 
 export default function SalesLayout({ children }: { children: React.ReactNode }) {
-  return <div className={`${styles.salesVisual} ${reference.transactionRoot} ${voucherModal.voucherModalScope}`}>{children}<LatestVoucherScreen /></div>
+  return <div className={`${styles.salesVisual} ${matchStyles.match} ${reference.transactionRoot} ${voucherModal.voucherModalScope}`}>{children}<LatestVoucherScreen /></div>
 }
