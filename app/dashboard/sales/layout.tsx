ab3720ff75676be09f4074a4a10771f988667086
HBHATIA80 <61718992+HBHATIA80@users.noreply.github.com>
2026-09-01 17:28:06 +0530

    Apply mobile voucher styles to sales

--- app/dashboard/sales/layout.tsx
+++ app/dashboard/sales/layout.tsx
@@ -2,8 +2,9 @@ import styles from './sales.module.css'
 import matchStyles from './sales-purchase-match.module.css'
 import reference from '../transaction-voucher-reference.module.css'
 import voucherModal from '../latest-voucher-modal.module.css'
+import mobile from '../mobile-voucher.module.css'
 import LatestVoucherScreen from '../LatestVoucherScreen'
 
 export default function SalesLayout({ children }: { children: React.ReactNode }) {
-  return <div className={`${styles.salesVisual} ${matchStyles.match} ${reference.transactionRoot} ${voucherModal.voucherModalScope}`}>{children}<LatestVoucherScreen /></div>
+  return <div className={`${styles.salesVisual} ${matchStyles.match} ${reference.transactionRoot} ${voucherModal.voucherModalScope} ${mobile.mobileVoucher}`}>{children}<LatestVoucherScreen /></div>
 }
