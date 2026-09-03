45d4c9ea9304d30779ace3f773af1f6bd2cb1097
HBHATIA80 <61718992+HBHATIA80@users.noreply.github.com>
2026-09-03 09:22:14 +0530

    Match Sales mobile layout to Purchase

--- app/dashboard/sales/layout.tsx
+++ app/dashboard/sales/layout.tsx
@@ -4,8 +4,9 @@ import reference from '../transaction-voucher-reference.module.css'
 import voucherModal from '../latest-voucher-modal.module.css'
 import mobile from '../mobile-voucher.module.css'
 import mobileFinal from '../mobile-voucher-final.module.css'
+import mobileMatch from './sales-mobile-match.module.css'
 import LatestVoucherScreen from '../LatestVoucherScreen'
 
 export default function SalesLayout({ children }: { children: React.ReactNode }) {
-  return <div className={`${styles.salesVisual} ${matchStyles.match} ${reference.transactionRoot} ${voucherModal.voucherModalScope} ${mobile.mobileVoucher} ${mobileFinal.mobileVoucherFinal}`}>{children}<LatestVoucherScreen /></div>
+  return <div className={`${styles.salesVisual} ${matchStyles.match} ${reference.transactionRoot} ${voucherModal.voucherModalScope} ${mobile.mobileVoucher} ${mobileFinal.mobileVoucherFinal} ${mobileMatch.salesMobileMatch}`}>{children}<LatestVoucherScreen /></div>
 }
