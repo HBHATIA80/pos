d5614f74c6136386f28e625948c251321f73c444
HBHATIA80 <61718992+HBHATIA80@users.noreply.github.com>
2026-09-03 09:09:16 +0530

    Apply final mobile POS redesign to Purchase

--- app/dashboard/purchases/layout.tsx
+++ app/dashboard/purchases/layout.tsx
@@ -2,8 +2,9 @@ import reference from '../transaction-voucher-reference.module.css'
 import typography from './purchase-typography.module.css'
 import voucherModal from '../latest-voucher-modal.module.css'
 import mobile from '../mobile-voucher.module.css'
+import mobileFinal from '../mobile-voucher-final.module.css'
 import LatestVoucherScreen from '../LatestVoucherScreen'
 
 export default function PurchasesLayout({ children }: { children: React.ReactNode }) {
-  return <div className={`${reference.transactionRoot} ${typography.purchaseTypography} ${voucherModal.voucherModalScope} ${mobile.mobileVoucher}`}>{children}<LatestVoucherScreen /></div>
+  return <div className={`${reference.transactionRoot} ${typography.purchaseTypography} ${voucherModal.voucherModalScope} ${mobile.mobileVoucher} ${mobileFinal.mobileVoucherFinal}`}>{children}<LatestVoucherScreen /></div>
 }
