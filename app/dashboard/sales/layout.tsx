8dbf6a95d573811b93b5015d0cc3d3ba8f2a5efd
HBHATIA80 <61718992+HBHATIA80@users.noreply.github.com>
2026-09-03 09:09:11 +0530

    Apply final mobile POS redesign to Sales

--- app/dashboard/sales/layout.tsx
+++ app/dashboard/sales/layout.tsx
@@ -3,8 +3,9 @@ import matchStyles from './sales-purchase-match.module.css'
 import reference from '../transaction-voucher-reference.module.css'
 import voucherModal from '../latest-voucher-modal.module.css'
 import mobile from '../mobile-voucher.module.css'
+import mobileFinal from '../mobile-voucher-final.module.css'
 import LatestVoucherScreen from '../LatestVoucherScreen'
 
 export default function SalesLayout({ children }: { children: React.ReactNode }) {
-  return <div className={`${styles.salesVisual} ${matchStyles.match} ${reference.transactionRoot} ${voucherModal.voucherModalScope} ${mobile.mobileVoucher}`}>{children}<LatestVoucherScreen /></div>
+  return <div className={`${styles.salesVisual} ${matchStyles.match} ${reference.transactionRoot} ${voucherModal.voucherModalScope} ${mobile.mobileVoucher} ${mobileFinal.mobileVoucherFinal}`}>{children}<LatestVoucherScreen /></div>
 }
